9605f93c1e4bc7c530713e484522d56f0c00e7c8
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2025-03-20 20:54:26 +0100

    feat(Element): id have been added as well as a class constructor and a abstract display function

--- ReMind/src/main/java/fr/ynov/remind/domain/Element.java
+++ ReMind/src/main/java/fr/ynov/remind/domain/Element.java
@@ -1,6 +1,13 @@
 package main.java.fr.ynov.remind.domain;
 
+import javax.swing.*;
 import java.util.Calendar;
+public abstract class Element {
+    protected Element(int id, String name, Calendar date) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+    }
 
 public class Element {
     public String getName() {
@@ -19,7 +26,9 @@ public class Element {
         this.date = date;
     }
 
+    private final int id;
     protected String name;
     protected Calendar date;
     // put tags list
+    public abstract JPanel display();
 }
