a07c0638cc5a28562485076d15d38db6773324ee
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2025-03-20 20:52:56 +0100

    feat(ApplicationRunner): call to MainPage function have been added to launch the program (tests still in progress)

---
+++ ReMind/src/main/java/fr/ynov/remind/main/ApplicationRunner.java
@@ -0,0 +1,9 @@
+package main.java.fr.ynov.remind.main;
+
+import main.java.fr.ynov.remind.gui.MainPage;
+
+public class ApplicationRunner {
+    public static void main(String[] args) {
+        new MainPage().mainPage();
+    }
+}
