21605ddb4f9cde8aa9e3f6640840d1d8f3d6f879
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2025-03-25 16:42:41 +0100

    feat(ApplicationRunner): element factory object is now requested to launch the main page in order to access the data

--- ReMind/src/main/java/fr/ynov/remind/main/ApplicationRunner.java
+++ ReMind/src/main/java/fr/ynov/remind/main/ApplicationRunner.java
@@ -1,9 +1,10 @@
 package main.java.fr.ynov.remind.main;
 
+import main.java.fr.ynov.remind.factory.ElementsFactory;
 import main.java.fr.ynov.remind.gui.MainPage;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        new MainPage().mainPage();
+        new MainPage().mainPage(new ElementsFactory());
     }
 }
