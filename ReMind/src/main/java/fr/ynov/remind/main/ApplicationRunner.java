5f917050864e398ed88272b25c56e8a3124c265b
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2025-03-31 23:52:47 +0200

    feat(ApplicationRunner): ApplicationRunner have been modified to call and load data from the json file

--- ReMind/src/main/java/fr/ynov/remind/main/ApplicationRunner.java
+++ ReMind/src/main/java/fr/ynov/remind/main/ApplicationRunner.java
@@ -1,10 +1,13 @@
 package main.java.fr.ynov.remind.main;
 
 import main.java.fr.ynov.remind.factory.ElementsFactory;
+import main.java.fr.ynov.remind.factory.JsonHandler;
 import main.java.fr.ynov.remind.gui.MainPage;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        new MainPage().mainPage(new ElementsFactory());
+        ElementsFactory elementsFactory = new ElementsFactory();
+        JsonHandler.loadDataFromJSon(elementsFactory);
+        new MainPage().mainPage(elementsFactory);
     }
 }
