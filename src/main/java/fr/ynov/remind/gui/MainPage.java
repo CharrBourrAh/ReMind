b7c19167d255875e111c949b4ec3f6f92e6519ee
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2025-04-02 21:33:10 +0200

    feat(JsonHandler): java doc added

--- src/main/java/fr/ynov/remind/gui/MainPage.java
+++ src/main/java/fr/ynov/remind/gui/MainPage.java
@@ -24,6 +24,15 @@ public class MainPage extends JFrame {
     private JPanel mainPagePanel;
     private JScrollPane reminderScrollPanel = new JScrollPane();
 
+    /**
+     * Constructor for MainPage.
+     * This method initializes the main page of the application
+     * by setting up the different components to correctly show the stored elements.
+     * It creates a CardLayout to switch between the main page and the element page.
+     * It also creates a button to add new elements and displays the existing elements in a scrollable panel.
+     *
+     * @param elementsFactory The factory that creates and manages the different elements.
+     */
     public void mainPage(ElementsFactory elementsFactory) {
         this.elementsFactory = elementsFactory;
         setTitle("ReMind");
@@ -58,6 +67,13 @@ public class MainPage extends JFrame {
         mainPagePanel.setVisible(true);
     }
 
+    /**
+     * This method is used to display the elements in the main page and adding them into a scrollable list.
+     *
+     * @param factory The factory that creates and manages the elements
+     *                (in this case, will be used to access the different stored elements).
+     * @param panel   The panel where the stored elements will be displayed.
+     */
     private void displayElements(ElementsFactory factory, JPanel panel) {
         JPanel elementsPanel = new JPanel();
         elementsPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
@@ -75,6 +91,10 @@ public class MainPage extends JFrame {
         panel.add(reminderScrollPanel, BorderLayout.CENTER);
     }
 
+    /**
+     * This method is used to show the main page of the application.
+     * It refreshes the scroll panel to display the updated list of elements.
+     */
     public void showMainPage() {
         mainPagePanel.remove(reminderScrollPanel);
         displayElements(elementsFactory, mainPagePanel);
